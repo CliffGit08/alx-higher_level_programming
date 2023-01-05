5cc8b4c1bf3c96e6b8f098f15a2b05c404f5f56c
Cliffort Mosebi Mmakola <c8cliffort@gmail.com>
2023-01-05 01:08:59 -0800

    2023

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
 """Defines a Rectangle class."""
 
+
 class Rectangle:
-
-"""Represent a rectangle.
-   This class does not have any attributes or methods yet,
-   so we use the `pass` statement as a placeholder."""
-
+    """Represent a rectangle."""
     pass
